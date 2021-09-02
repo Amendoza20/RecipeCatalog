3dfe2e5d10663b9bf4e20a18b188054aac62e864
Amendoza20 <20.angel.mendoza@gmail.com>
2021-09-01 22:13:04 -0400

    first commit. Set up Spring

---
+++ src/test/java/com/example/recipeCatalog/RecipeCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipeCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
